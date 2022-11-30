a4c45c4ed8ad408cc974a1647d81a548a0c535f2
Daniel Jilg <daniel@telemetrydeck.com>
2022-11-30 11:24:00 +0100

    Initialize TelemetryDeck

--- PizzaMaker/PizzaMaker/PizzaMakerApp.swift
+++ PizzaMaker/PizzaMaker/PizzaMakerApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import TelemetryClient
 
 @main
 struct PizzaMakerApp: App {
@@ -13,5 +14,11 @@ struct PizzaMakerApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    init() {
+        let configuration = TelemetryManagerConfiguration(appID: "02C49F2E-F3FF-4715-BBB6-C381F9541905")
+        // optional: modify the configuration here
+        TelemetryManager.initialize(with: configuration)
     }
 }
