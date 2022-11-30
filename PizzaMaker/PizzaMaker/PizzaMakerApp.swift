6933421b40f9a08bd7d4db69f7c0a28f6383b093
Daniel Jilg <daniel@telemetrydeck.com>
2022-11-30 11:41:20 +0100

    Send configuration on launch

--- PizzaMaker/PizzaMaker/PizzaMakerApp.swift
+++ PizzaMaker/PizzaMaker/PizzaMakerApp.swift
@@ -10,6 +10,8 @@ import TelemetryClient
 
 @main
 struct PizzaMakerApp: App {
+    @AppStorage("DisplayUnit") private var displayUnit = "Metric"
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -20,5 +22,7 @@ struct PizzaMakerApp: App {
         let configuration = TelemetryManagerConfiguration(appID: "02C49F2E-F3FF-4715-BBB6-C381F9541905")
         // optional: modify the configuration here
         TelemetryManager.initialize(with: configuration)
+        
+        TelemetryManager.send("appLaunchedRegularly", with: ["DisplayUnit" : displayUnit])
     }
 }
